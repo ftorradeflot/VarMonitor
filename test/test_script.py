dfe644df5465c5f6c1363d3e6eba5f18a6394941
Francesc Torradeflot <torradeflot@pic.es>
2018-01-19 11:52:16 +0100

    Some minor changes

--- test/test_script.py
+++ test/test_script.py
@@ -2,18 +2,22 @@ import os
 import json
 import time
 import random
+import sys
 
-N_ITER = 1000000
+MAX_LEN = 1000000
 TMP_FILE = '/tmp/kk.json'
+N_ITER = 100
 
-while True:
+for i in range(N_ITER):
     if os.path.exists(TMP_FILE):
         with open(TMP_FILE, 'r') as f:
             l = json.load(f)
     else:
         l = []
     
-    l = [i**2 for i in range(random.randint(0, N_ITER))]
+    l = [i**2 for i in range(random.randint(0, MAX_LEN))]
     
     with open(TMP_FILE, 'w') as f:
         json.dump(l, f)
+
+sys.exit(0)
